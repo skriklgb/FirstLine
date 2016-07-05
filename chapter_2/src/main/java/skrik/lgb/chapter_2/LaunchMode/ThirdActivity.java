7ec11a227d1c828dfa27b813b93f591510da9c9a
skriklgb <1071282757@qq.com>
2016-07-05 09:01:16 +0800

    第2章-2.6.2　随时随地退出程序

--- chapter_2/src/main/java/skrik/lgb/chapter_2/LaunchMode/ThirdActivity.java
+++ chapter_2/src/main/java/skrik/lgb/chapter_2/LaunchMode/ThirdActivity.java
@@ -2,11 +2,16 @@ package skrik.lgb.chapter_2.LaunchMode;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import skrik.lgb.chapter_2.R;
+import skrik.lgb.chapter_2.skill.ActivityCollector;
 import skrik.lgb.chapter_2.skill.BaseActivity;
 
 public class ThirdActivity extends BaseActivity {
+
+    private Button mBt_finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,13 @@ public class ThirdActivity extends BaseActivity {
         Log.d("ThirdActivity返回栈的id","ThirdActivity返回栈的id是："+getTaskId());
         setContentView(R.layout.activity_third2);
 
+        mBt_finish = (Button) findViewById(R.id.bt_finish);
 
+        mBt_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCollector.finishAll();
+            }
+        });
     }
 }
--- chapter_2/src/main/java/skrik/lgb/chapter_2/skill/BaseActivity.java
+++ chapter_2/src/main/java/skrik/lgb/chapter_2/skill/BaseActivity.java
@@ -9,6 +9,14 @@ public class BaseActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("当前的活动为：",getClass().getSimpleName());
+
+        ActivityCollector.addActivity(this);  //将当前正在创建的活动添加到活动管理器里
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);   //将一个马上要销毁的活动从活动管 理器里移除。
+
+    }
 }
