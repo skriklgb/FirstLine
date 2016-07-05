aebb89624365c0997ccf0c48af833c1e4b0dfcfb
skriklgb <1071282757@qq.com>
2016-07-05 14:12:46 +0800

    第3章-3.2.5　ProgressBar

--- chapter_3/src/main/java/skrik/lgb/chapter_3/widget/WidgetActivity.java
+++ chapter_3/src/main/java/skrik/lgb/chapter_3/widget/WidgetActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import skrik.lgb.chapter_3.R;
@@ -16,6 +17,9 @@ public class WidgetActivity extends Activity implements View.OnClickListener {
     private EditText mEt_edittext;
     private ImageView mIv_image;
     private Button mBt_button2;
+    private Button mBt_button3;
+    private ProgressBar mPb_pro;
+    private Button mBt_button4;
 
 
     @Override
@@ -27,9 +31,14 @@ public class WidgetActivity extends Activity implements View.OnClickListener {
         mEt_edittext = (EditText) findViewById(R.id.et_edittext);
         mIv_image = (ImageView) findViewById(R.id.iv_image);
          mBt_button2 = (Button) findViewById(R.id.bt_button2);
+        mPb_pro = (ProgressBar) findViewById(R.id.pb_pro);
+        mBt_button3 = (Button) findViewById(R.id.bt_button3);
+        mBt_button4 = (Button) findViewById(R.id.bt_button4);
 
         mBt_button.setOnClickListener(this);
         mBt_button2.setOnClickListener(this);
+        mBt_button3.setOnClickListener(this);
+        mBt_button4.setOnClickListener(this);
 
         
     }
@@ -45,8 +54,21 @@ public class WidgetActivity extends Activity implements View.OnClickListener {
             case R.id.bt_button2:
                 mIv_image.setImageResource(R.mipmap.jelly_bean);
                 break;
-        
-             default:
+
+            case R.id.bt_button3:
+                if (mPb_pro.getVisibility() == View.GONE){
+                    mPb_pro.setVisibility(View.VISIBLE);
+                } else {
+                    mPb_pro.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.bt_button4:
+                int progress = mPb_pro.getProgress();
+                progress = progress+10;
+                mPb_pro.setProgress(progress);
+                break;
+
+            default:
                  break;
              }
 
