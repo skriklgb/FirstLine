201d3a48d8f7427fb9695ca5570f72a044cba14f
skriklgb <1071282757@qq.com>
2016-07-08 07:32:01 +0800

    第5章  广播(Broadcast)-5.2.1　动态注册监听网络变化

--- chapter_5/src/main/java/skrik/lgb/chapter_5/MainActivity.java
+++ chapter_5/src/main/java/skrik/lgb/chapter_5/MainActivity.java
@@ -1,13 +1,53 @@
 package skrik.lgb.chapter_5;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private IntentFilter mIntentFilter;
+    private NetvorkChangeReceive mNetvorkChangeReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        mNetvorkChangeReceive = new NetvorkChangeReceive();
+        registerReceiver(mNetvorkChangeReceive,mIntentFilter);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mNetvorkChangeReceive);
+    }
+
+    class NetvorkChangeReceive extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            ConnectivityManager connectionManager  = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is可用", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(context, "network is 不可用", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
