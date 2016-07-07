f5cde5ab4038babc50db729fe763bc3eae272853
skriklgb <1071282757@qq.com>
2016-07-07 14:46:13 +0800

    第4章  碎片（Fragment）-4.2.1碎片的简单用法

---
+++ chapter_4/src/main/java/skrik/lgb/chapter_4/LeftFragment.java
@@ -0,0 +1,19 @@
+package skrik.lgb.chapter_4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LeftFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_left,container,false);
+        return view;
+    }
+}
