c0accb876149b3694a2b375d2c2732a02d46ebc7
skriklgb <1071282757@qq.com>
2016-07-11 09:24:39 +0800

    第6章  数据存储-6.3.2　从SharedPreferences中读取数据

--- chapter_6/src/main/java/skrik/lgb/chapter_6/SharedPreferences/SharedPreferencesActivity.java
+++ chapter_6/src/main/java/skrik/lgb/chapter_6/SharedPreferences/SharedPreferencesActivity.java
@@ -3,6 +3,7 @@ package skrik.lgb.chapter_6.SharedPreferences;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +12,7 @@ import skrik.lgb.chapter_6.R;
 public class SharedPreferencesActivity extends Activity {
 
     private Button mSave_data;
+    private Button mRestore_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,8 @@ public class SharedPreferencesActivity extends Activity {
         setContentView(R.layout.activity_shared_preferences);
 
         mSave_data = (Button) findViewById(R.id.save_data);
+        mRestore_data = (Button) findViewById(R.id.restore_data);
+
         mSave_data.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +30,19 @@ public class SharedPreferencesActivity extends Activity {
                 editor.putInt("age",28);
                 editor.putBoolean("married",false);
                 editor.commit();
+            }
+        });
+
+        mRestore_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pre = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pre.getString("name", "");
+                int age = pre.getInt("age", 0);
+                boolean married = pre.getBoolean("married", false);
+                Log.d("从SharedPreferences中读取数","name is"+name);
+                Log.d("从SharedPreferences中读取数","age is"+age);
+                Log.d("从SharedPreferences中读取数","married is"+married);
             }
         });
 
