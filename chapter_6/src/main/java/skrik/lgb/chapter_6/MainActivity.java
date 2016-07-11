b26f92d84f16cfcfe759cbe00cc59e431eddbf3a
skriklgb <1071282757@qq.com>
2016-07-11 08:00:27 +0800

    第6章  数据存储-6.2.2　从文件中读取数据

--- chapter_6/src/main/java/skrik/lgb/chapter_6/MainActivity.java
+++ chapter_6/src/main/java/skrik/lgb/chapter_6/MainActivity.java
@@ -3,11 +3,16 @@ package skrik.lgb.chapter_6;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends Activity {
@@ -18,9 +23,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mEdit = (EditText) findViewById(R.id.edit);
 
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            mEdit.setText(inputText);
+            mEdit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String load() {
+        FileInputStream in =null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        }  catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return  content.toString();
     }
 
     @Override
