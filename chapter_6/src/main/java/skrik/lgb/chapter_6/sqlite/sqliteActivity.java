7ba8ee61b88f7622ea0b8c9cbd40495483a46168
skriklgb <1071282757@qq.com>
2016-07-12 10:23:24 +0800

    第6章  数据存储-6.4.3　添加数据

--- chapter_6/src/main/java/skrik/lgb/chapter_6/sqlite/sqliteActivity.java
+++ chapter_6/src/main/java/skrik/lgb/chapter_6/sqlite/sqliteActivity.java
@@ -1,5 +1,7 @@
 package skrik.lgb.chapter_6.sqlite;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +13,7 @@ public class sqliteActivity extends AppCompatActivity {
 
     private MyDatabaseHelper mMyDatabaseHelper;
     private Button mCreate_database;
+    private Button mAdd_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +22,32 @@ public class sqliteActivity extends AppCompatActivity {
 
         mMyDatabaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 3);
         mCreate_database = (Button) findViewById(R.id.create_database);
+        mAdd_data = (Button) findViewById(R.id.add_data);
 
         mCreate_database.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mMyDatabaseHelper.getWritableDatabase();
+            }
+        });
+
+        mAdd_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = mMyDatabaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                // 开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book",null,values);  // 插入第一条数据
+                // 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); // 插入第二条数据
             }
         });
     }
