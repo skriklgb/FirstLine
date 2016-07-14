751896193e2ad641d0e029aed605b22f987ea79f
skriklgb <1071282757@qq.com>
2016-07-14 19:33:52 +0800

    第8章  手机多媒体-8.2.3　发送短信

--- chapter_8/src/main/java/skrik/lgb/chapter_8/Message/MsgActivity.java
+++ chapter_8/src/main/java/skrik/lgb/chapter_8/Message/MsgActivity.java
@@ -1,13 +1,19 @@
 package skrik.lgb.chapter_8.Message;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import skrik.lgb.chapter_8.R;
 
@@ -20,6 +26,11 @@ public class MsgActivity extends Activity {
 
     private TextView mSender;
     private TextView mContent;
+    private EditText mTo;
+    private EditText mMsg_input;
+    private Button mSend;
+    private SendStatusReceiver mSendStatusReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +39,50 @@ public class MsgActivity extends Activity {
 
         mSender = (TextView) findViewById(R.id.sender);
         mContent = (TextView) findViewById(R.id.content);
+        mTo = (EditText) findViewById(R.id.to);
+        mMsg_input = (EditText) findViewById(R.id.msg_input);
+        mSend = (Button) findViewById(R.id.send);
 
         receiveFilter = new IntentFilter();
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         receiveFilter.setPriority(100);
         messageReceiver = new MessageReceiver();
         registerReceiver(messageReceiver, receiveFilter);
+
+        receiveFilter.addAction("SENT_SMS_ACTION");
+        mSendStatusReceiver = new SendStatusReceiver();
+        registerReceiver(mSendStatusReceiver, receiveFilter);
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                Intent sendintent = new Intent("SENT_SMS_ACTION");
+                PendingIntent pi = PendingIntent.getActivity(MsgActivity.this, 0, sendintent, 0);
+                smsManager.sendTextMessage(mTo.getText().toString(),null,mMsg_input.getText().toString(),pi,null);
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(messageReceiver);
+        unregisterReceiver(mSendStatusReceiver);
+    }
+
+    public  class SendStatusReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (getResultCode() == RESULT_OK){
+                // 短信发送成功
+                Toast.makeText(context, "Send succeeded",Toast.LENGTH_LONG).show();
+            } else{
+                // 短信发送失败
+                Toast.makeText(context, "Send failed",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public class MessageReceiver extends BroadcastReceiver {
@@ -61,4 +104,5 @@ public class MsgActivity extends Activity {
             abortBroadcast();
         }
     }
+
 }
