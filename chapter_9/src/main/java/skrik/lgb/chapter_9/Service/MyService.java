32ece0366e0d00d9164b8b005eb3e5558a6d03ba
skriklgb <1071282757@qq.com>
2016-07-15 19:45:09 +0800

    第9章  服务(Service)-9.3.3　活动和服务进行通信

--- chapter_9/src/main/java/skrik/lgb/chapter_9/Service/MyService.java
+++ chapter_9/src/main/java/skrik/lgb/chapter_9/Service/MyService.java
@@ -2,15 +2,29 @@ package skrik.lgb.chapter_9.Service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
 
     }
 
--- chapter_9/src/main/java/skrik/lgb/chapter_9/Service/ServiceActivity.java
+++ chapter_9/src/main/java/skrik/lgb/chapter_9/Service/ServiceActivity.java
@@ -1,7 +1,10 @@
 package skrik.lgb.chapter_9.Service;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,21 @@ public class ServiceActivity extends AppCompatActivity implements View.OnClickLi
 
     private Button mStart_service;
     private Button mStop_service;
+    private Button mBind_service;
+    private Button mUnbind_service;
+
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +39,11 @@ public class ServiceActivity extends AppCompatActivity implements View.OnClickLi
         mStop_service = (Button) findViewById(R.id.stop_service);
         mStart_service.setOnClickListener(this);
         mStop_service.setOnClickListener(this);
+
+        mBind_service = (Button) findViewById(R.id.bind_service);
+        mUnbind_service = (Button) findViewById(R.id.unbind_service);
+        mBind_service.setOnClickListener(this);
+        mUnbind_service.setOnClickListener(this);
     }
 
     @Override
@@ -29,13 +52,19 @@ public class ServiceActivity extends AppCompatActivity implements View.OnClickLi
              case R.id.start_service:
                  Intent startintent = new Intent(this,MyService.class);
                  startService(startintent);
-
                  break;
 
             case R.id.stop_service:
                 Intent stopintent = new Intent(this,MyService.class);
                 stopService(stopintent);
+                break;
 
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE); // 绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection); // 解绑服务
                 break;
 
              default:
