7f971dc61c9bd58315a60739ac6897ba83376b56
skriklgb <1071282757@qq.com>
2016-07-18 07:27:39 +0800

    第11章  基于位置的服务　11.3.2　对经纬度进行解析

--- chapter_11/src/main/java/skrik/lgb/chapter_11/MainActivity.java
+++ chapter_11/src/main/java/skrik/lgb/chapter_11/MainActivity.java
@@ -6,13 +6,24 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
-
+    public static final int SHOW_LOCATION = 0;
     private TextView positionTextView;
     private LocationManager locationManager;
     private String provider;
@@ -66,8 +77,56 @@ public class MainActivity extends Activity {
             showLocation(location);
         }
     };
-    private void showLocation(Location location) {
-        String currentPosition = "纬度是：" + location.getLatitude() + "\n"+ "经度是：" + location.getLongitude();
-        positionTextView.setText(currentPosition);
+
+    private void showLocation(final Location location) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 组装反向地理编码的接口地址
+                    StringBuilder url = new StringBuilder();
+                    url.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+                    url.append(location.getLatitude()).append(",");
+                    url.append(location.getLongitude());
+                    url.append("&sensor=false");
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet(url.toString());
+                    // 在请求消息头中指定语言，保证服务器会返回中文数据
+                    httpGet.addHeader("Accept-Language", "zh-CN");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity,"utf-8");
+                        JSONObject jsonObject = new JSONObject(response);
+                        // 获取results节点下的位置信息
+                        JSONArray resultArray = jsonObject.getJSONArray("results");
+                        if (resultArray.length() > 0) {
+                            JSONObject subObject = resultArray.getJSONObject(0);
+                            // 取出格式化后的位置信息
+                            String address = subObject.getString("formatted_address");
+                            Message message = new Message();
+                            message.what = SHOW_LOCATION;
+                            message.obj = address;
+                            handler.sendMessage(message);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_LOCATION:
+                    String currentPosition = (String) msg.obj;
+                    positionTextView.setText(currentPosition);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
