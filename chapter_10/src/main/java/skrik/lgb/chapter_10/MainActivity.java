140002b1eff874d8b6c0eda09bc968615b319a1f
skriklgb <1071282757@qq.com>
2016-07-17 09:24:36 +0800

    第10章  网络技术　10.1　WebView的用法

--- chapter_10/src/main/java/skrik/lgb/chapter_10/MainActivity.java
+++ chapter_10/src/main/java/skrik/lgb/chapter_10/MainActivity.java
@@ -2,12 +2,28 @@ package skrik.lgb.chapter_10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWeb_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWeb_view = (WebView) findViewById(R.id.web_view);
+        mWeb_view.getSettings().setJavaScriptEnabled(true);
+        mWeb_view.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);// 根据传入的参数再去加载新的网页
+                return true; // 表示当前WebView可以处理打开新网页的请求，不用借助系统浏览器
+
+            }
+        });
+        mWeb_view.loadUrl("http://www.baidu.com");
     }
 }
