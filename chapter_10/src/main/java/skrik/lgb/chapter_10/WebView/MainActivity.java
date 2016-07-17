209c89a86bd374e5ac5e49f826eb10f9665f2a99
skriklgb <1071282757@qq.com>
2016-07-17 10:28:49 +0800

    第10章  网络技术　10.2.1　使用HttpURLConnection

--- chapter_10/src/main/java/skrik/lgb/chapter_10/WebView/MainActivity.java
+++ chapter_10/src/main/java/skrik/lgb/chapter_10/WebView/MainActivity.java
@@ -1,9 +1,11 @@
-package skrik.lgb.chapter_10;
+package skrik.lgb.chapter_10.WebView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import skrik.lgb.chapter_10.R;
 
 public class MainActivity extends AppCompatActivity {
 
